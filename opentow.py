050427826dd1d3dbe4b76c67f6917cd87d6e3df0
trevor.rowe <trevor.rowe@cgi.com>
2019-07-08 11:28:54 -0400

    Moved db file existence check into main()

--- opentow.py
+++ opentow.py
@@ -2,15 +2,15 @@ import queries
 import api
 import os
 import xml.etree.ElementTree as Et
-
-conf_tree = Et.parse('config')
-conf_root = conf_tree.getroot()
-
-if not os.path.exists('openToW.sqlite'):
-    queries.setup(conf_root)
+from timer import *
 
 
 def main():
+    conf_tree = Et.parse('config')
+    conf_root = conf_tree.getroot()
+
+    if not os.path.exists('openToW.sqlite'):
+        queries.setup(conf_root)
     queries.set_active_sectors()
     api.start()
 
