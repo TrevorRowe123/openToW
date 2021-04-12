5ace814a07e58c7b50cf56c399685a7fa2a03a5e
werro123 <tr968114@ohio.edu>
2021-04-12 19:09:16 -0400

    Making sectors API more RESTful

--- lib/api/sector.py
+++ lib/api/sector.py
@@ -2,31 +2,43 @@ import cherrypy
 from lib import queries
 
 
-@cherrypy.expose
-@cherrypy.tools.json_in()
-@cherrypy.tools.json_out()
 class Sector(object):
 
-    def GET(self, **params):
-        if 'id' in params:
-            return {
-                'owner': queries.get_sector_owner(params['id']),
-                'scores': queries.get_sector_scores(params['id']),
-                'active': queries.sector_is_active(params['id'])
-            }
-        else:
-            return queries.get_sectors()
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @cherrypy.tools.allow(methods=["GET"])
+    def all(self):
+        return queries.get_sectors()
 
-    @cherrypy.tools.accept(media="application/json")
-    def POST(self):
-        j_request = cherrypy.request.json
-        if not queries.sector_is_active(j_request['sector']):
+    def get_sector(self, sector_id):
+        return {
+            'owner': queries.get_sector_owner(sector_id),
+            'scores': queries.get_sector_scores(sector_id),
+            'active': queries.sector_is_active(sector_id)
+        }
+
+    def post_sector(self, j_request, sector_id):
+        if not queries.sector_is_active(sector_id):
             raise cherrypy.HTTPError(403, "Forbidden: Specified Sector is not active")
 
-        if not queries.token_match(j_request['sector'], j_request['token']):
+        if not queries.token_match(sector_id, j_request['token']):
             raise cherrypy.HTTPError(401)
         queries.update_score(
-            j_request['sector'],
+            int(sector_id),
             j_request['faction'],
             j_request['score']
         )
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.accept(media="application/json")
+    @cherrypy.tools.allow(methods=["GET", "POST"])
+    def default(self, sector_id):
+        if cherrypy.request.method == "GET":
+            return self.get_sector(sector_id)
+
+        if cherrypy.request.method == "POST":
+            return self.post_sector(cherrypy.request.json, sector_id)
+
+
